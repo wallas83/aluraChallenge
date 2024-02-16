80bae3a815e3a66de62f38107db15fa21bd5797f
wallas <whuanca06@gmail.com>
2024-02-16 17:28:42 -0400

    adding encripting ans styles

--- src/encriptador.js
+++ src/encriptador.js
@@ -1,53 +1,70 @@
 
 const textArea = document.getElementById('textarea');
 const btnEncriptar = document.getElementById('encriptar');
-const divSalida = document.getElementById('salida');
 const btnDesencriptar = document.getElementById('desencriptar')
-const btCopiar = document.getElementById('copiar');
-
-let valorTextarea = '';
+const btnCopiar = document.getElementById('copiar');
+const spnText = document.getElementById('alerta');
+const lbSalida = document.getElementById('salida');
+let valorTextarea = textArea.value;
 
 btnEncriptar.disabled = true;
 btnDesencriptar.disabled = true;
-
+btnCopiar.disabled = true;
+// funcion del textarea
 textArea.addEventListener('input', function () {
 
     verificaBotones();
+    spnText.innerText = 'No ingrese mayúsculas ni acentos';
+    spnText.classList.remove('peligro');
     valorTextarea = textArea.value
     const tieneMayusculaOAccentuada = /[A-Z\u00C0-\u00FF]/.test(valorTextarea);
 
-    if (tieneMayusculaOAccentuada) {
-        console.log('éntro a mayusculas y acentos');
-        alert('tiene mayusculas o acentos')
-        textArea.value = "";
-        return;
-    } 
-});
 
+    if (tieneMayusculaOAccentuada) {
+        // alert('tiene mayusculas o acentos');
+        spnText.innerText = 'Escribiste mayúsculas o acentos';
+        spnText.classList.add('peligro')
+
+        btnEncriptar.disabled = true;
+        btnDesencriptar.disabled = true;
+        btnCopiar.disabled = true;
+        return;
+    }
+});
+// evento del boton encriptar
 btnEncriptar.addEventListener('click', function () {
 
+    lbSalida.innerText = textArea.value;
     encriptar(valorTextarea);
-    divSalida.textContent = valorTextarea
-    console.log(valorTextarea);
-    // btnDesencriptar.disabled = false;
-    textArea.value = "";
+
+
     verificaBotones();
-    // btnEncriptar.disabled = true;
+    textArea.value = "";
 })
 
 
 btnDesencriptar.addEventListener('click', function () {
 
     // btnEncriptar.disabled = false;
-    const valorTextarea = textArea.value;
 
     desencriptar(valorTextarea);
-    divSalida.textContent = valorTextarea
-    console.log(valorTextarea);
-
-    textArea.value = "";
+    // inicializa los datos de entrada del text area despues de hacer click al bton desencriptar
+    // textArea.value = "";
     verificaBotones();
+    textArea.value = "";
 
+});
+btnCopiar.addEventListener('click', function () {
+    console.log(`dentro de copiar ${lbSalida.innerText}`);
+    navigator.clipboard.writeText(lbSalida.innerText).then(() => {
+        console.log('Content copied to clipboard');
+        // alert(`Contenido copiado: ${lbSalida.innerText}`);
+        showToast(`Texto Copiado !`, 1800);
+        lbSalida.innerText = "";
+    })
+        .catch(() => {
+            alert('contenido no copiado');
+        });
 })
 
 const verificaBotones = () => {
@@ -55,20 +72,67 @@ const verificaBotones = () => {
 
         btnEncriptar.disabled = true;
         btnDesencriptar.disabled = true;
-        
+        btnCopiar.disabled = true;
+        lbSalida.innerText = ' No se observa ningún dato todavia...!';
+        return;
     }
 
     if (textArea.value.trim() !== '') {
 
         btnEncriptar.disabled = false;
         btnDesencriptar.disabled = false;
-       
+        btnCopiar.disabled = false;
+
+        return;
     }
 }
 
-const encriptar = (cadena) => {
+const encriptar = (string) => {
+    console.log(`dentro de encriptar ${string}`);
+    const encriptar = {
+        a: 'ai',
+        e: 'enter',
+        i: 'imes',
+        o: 'ober',
+        u: 'ufat',
+  
+    }
+    let encriptado =  parse(string, encriptar);
+    lbSalida.innerText = encriptado;
 
 }
 const desencriptar = (cadena) => {
+    const desencriptar = {
+
+        ai: 'a',
+        enter: 'e',
+        imes: 'i',
+        ober: 'o',
+        ufat: 'u'
+    }
+    
+   let desencriptado = parse(cadena, desencriptar);
+   lbSalida.innerText = desencriptado;
 
 }
+const parse = (string, matchConjunto) => {
+    const regex = new RegExp(Object.keys(matchConjunto).join('|'), 'g');
+    let textoEncriptado = string.replace(regex, match => matchConjunto[match]);
+
+    return textoEncriptado;
+}
+
+function showToast(message, duration = 3000) {
+    const toastContainer = document.getElementById('toast-container');
+    
+    const toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.textContent = message;
+    
+    toastContainer.appendChild(toast);
+    
+    setTimeout(() => {
+      toast.remove();
+    }, duration);
+  }
+  
